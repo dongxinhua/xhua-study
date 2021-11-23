b38ee7b79aef174f48509daa9dec2e2304942cbf
dongxinhua <798616582@qq.com>
2021-11-23 16:49:36 +0800

    feat: 初始化及配置

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Count />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Count from './common/components/Count.vue';
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/common/components/Count.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="count">
+    <h2>
+      当前计数: {{ state.count }}
+    </h2>
+    <div>
+      请输入值 <input v-model="state.num" type="number" />
+    </div>
+    <div>
+      <button @click="increment(state.num)">
+        加文本框的值
+      </button>
+      <button @click="decrement(state.num)">
+        减文本框的值
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+const state = reactive({
+  count: 0,
+  num: 0,
+});
+
+const increment = (val) => {
+  state.count += val;
+};
+
+const decrement = (val) => {
+  state.count -= val;
+};
+</script>
+
+<style lang="less" scoped>
+  .count {
+    color: #1980ff;
+    
+    input {
+      margin-bottom: 22px;
+    }
+
+    button {
+      padding: 3px 10px;
+      margin-right: 10px;
+      border: 0;
+      border-radius: 15px;
+      background-color: #269577;
+      color: #fff;
+    }
+  }
+</style>
